1e7353c44e5bf9b83d9d03a07393af1e6ae8bb68
Antonio Gines <antonio57gines57@gmail.com>
2018-03-27 20:43:23 +0200

    Subida de repo web

---
+++ Projectos/__Conjunto/Tutoriales/tipilocks/nodeJS/preMade/mastodonte.js
@@ -0,0 +1,70 @@
+alert("mastodonte.js");
+
+/*
+var machoAlfa="Kaktura12";
+var name_Var=140;
+var edad_mierda;
+document.write(machoAlfa+": "+name_Var);
+*/
+
+
+function pedirNumero(){
+  var edad_mierda= prompt("Introduce tu edad por favor.");
+  checkAge(edad_mierda);
+}
+function checkAge(una_edad){
+  if(una_edad>18){
+    document.write("Felicidades!! eres un puto gilipollas mas.")
+  } else{
+    document.write("Felicidades!! eres un subnormal menos.")
+  }
+}
+
+
+
+document.write("eres un poco tonto verdad;");
+alert("mastodonte");
+
+
+
+function arriba(var name="CloseAlfa" type="number"){
+  if(CloseAlfa>20){
+    document.write("eres un viejo");
+  } else{
+      document.write("aun eres un poco joven");
+  }
+
+
+  if(CloseAlfa>=18){
+    document.write("Mayor de edad chaval");
+  } else{
+    document.write("Puto joven de mierda");
+  }
+}
+
+/*
+if(zeroDays<name_Var){
+  document.write("falso, que eres falso")
+} else{
+  document.write("verdad, eres mas bueno que jesulines");
+}
+*/
+
+
+/*
+do {
+    zeroDays=input();
+} while (zeroDays>name_Var);
+*/
+
+/*
+var _arrayZero= {1,7,98,5,4,5,5,4,5,4,2,5,4};
+for (var i = 0; i < $arrayZero.length; i++) {
+  if(i%2==0){
+    document.write($arrayZero[i]+" , ademas estamos en un indice impar.");
+  } else{
+    document.write($arrayZero[i]);
+  }
+}
+
+*/
